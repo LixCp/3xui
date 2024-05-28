b0894faa7877df45ab9f6f8774cc5192836d3631
rezazafarian81 <92252207+LixCp@users.noreply.github.com>
2024-05-28 20:48:33 +0330

    3

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from db import (
 )
 from xui import xui
 
-admins = [os.environ.get('ADMIN')]
+admins = os.environ.get('ADMIN')
 token = os.environ.get('TOKEN_BOT') 
 api_id = os.environ.get('APP_ID')
 api_hash = os.environ.get('APP_HASH')
